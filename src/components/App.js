a3a33c46749ce3ea734f33a488a287d39a839d95
developer119 <developer119korea@gmail.com>
2019-01-27 15:20:39 +0900

    할일 목록 지우기 기능 추가
    부모와 자식 요소 모두 온클릭 이벤트를 가지고 있으면 stopPropagation을 호출해야 한다

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   state = {
     input: '',
     todos: [
-      {id:0, text: '리액트 공부하기', done: true},
-      {id:1, text: '컴포넌트 스타일링 해보기', done: false }
+      { id: 0, text: '리액트 공부하기', done: true },
+      { id: 1, text: '컴포넌트 스타일링 해보기', done: false }
     ]
   }
 
@@ -52,7 +52,18 @@ class App extends Component {
       todos: [
         ...todos.slice(0, index),
         toggled,
-        ...todos.slice(index +1, todos.length)
+        ...todos.slice(index + 1, todos.length)
+      ]
+    });
+  }
+
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    const index = todos.findIndex(todo => todo.id === id);
+    this.setState({
+      todos: [
+        ...todos.slice(0, index),
+        ...todos.slice(index + 1, todos.length)
       ]
     });
   }
@@ -62,14 +73,14 @@ class App extends Component {
     const {
       handleChange,
       handleInsert,
-      handleToggle
-
+      handleToggle,
+      handleRemove
     } = this;
 
     return (
       <PageTemplate>
         <TodoInput onChange={handleChange} onInsert={handleInsert} value={input} />
-        <TodoList todos={todos} onToggle={handleToggle}/>
+        <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
       </PageTemplate>
     );
   }
